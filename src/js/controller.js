7df5ab1dd15453a317ac8707b413a1ef7f1703f6
Vince Chimento <vchimento@gmail.com>
2025-01-18 21:49:00 -0500

    Initialize Forkify project
    First commit of the project prior to changing private fields to protected fields.

---
+++ src/js/controller.js
@@ -0,0 +1,49 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1. Loading recipe
+    await model.loadRecipe(id);
+
+    // 2. Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
